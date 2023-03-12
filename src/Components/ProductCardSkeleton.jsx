3d3992f4ebfbc4dc1462d50fbe8a6016a2c326fc
aniketparmar29 <aniketparmar068@gmail.com>
2023-03-12 23:23:58 +0530

    ASD

---
+++ src/Components/ProductCardSkeleton.jsx
@@ -0,0 +1,23 @@
+import { Box, Skeleton } from '@chakra-ui/react';
+
+ const ProductCardSkeleton = () => {
+  return (
+    <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+      <Skeleton height="200px" />
+
+      <Box p="6">
+        <Skeleton height="20px" width="100%" my="2" />
+
+        <Skeleton height="40px" width="100%" my="2" />
+
+        <Skeleton height="20px" width="50%" my="2" />
+
+        <Skeleton height="20px" width="70%" my="2" />
+
+        <Skeleton height="30px" width="100%" my="2" />
+      </Box>
+    </Box>
+  );
+};
+
+export default ProductCardSkeleton;

6893c1cd4b574ae89c69d85f68597545f5bc645a
aniketparmar29 <aniketparmar068@gmail.com>
2023-03-20 14:49:26 +0530

    slidersaredynamic

--- src/Components/Offer.jsx
+++ src/Components/Offer.jsx
@@ -1,5 +1,5 @@
 
-import React, { useRef, useState ,useEffect} from "react";
+import React, {useEffect} from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 import Aos from "aos"
@@ -11,13 +11,6 @@ import "swiper/css/pagination";
 import SwiperCore,{Autoplay} from "swiper"
 import {useNavigate} from "react-router-dom"
 import "../Style/styles.css";
-import slider1 from "../Enhance/slider/slider2.jpg"
-import slider2 from "../Enhance/slider/slider3.jpg"
-import slider3 from "../Enhance/slider/slider1.jpg"
-import slider4 from "../Enhance/slider/slider4.jpg"
-import slider5 from "../Enhance/slider/slider5.jpeg"
-import slider6 from "../Enhance/slider/slider8.jpeg"
-import slider7 from "../Enhance/slider/slider7.jpeg"
 import { useDispatch,useSelector } from "react-redux";
 import {getSliders} from '../Redux/AdminReducer/actions'
 import{useMediaQuery} from "@chakra-ui/react"

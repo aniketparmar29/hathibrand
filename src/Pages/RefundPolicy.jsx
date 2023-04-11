93b6206d4f074ced1141667025bb23b286bd904f
aniketparmar29 <aniketparmar068@gmail.com>
2023-04-11 17:13:58 +0530

    AF

---
+++ src/Pages/RefundPolicy.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function RefundPolicy() {
+  return (
+    <div>
+        We want our customers to be completely satisfied with their purchases. If for any reason you are not satisfied, we offer a full refund or exchange within 30 days of purchase.
+
+To be eligible for a refund or exchange, the item must be returned in its original condition with all tags and packaging intact. We reserve the right to deny refunds or exchanges for items that show signs of wear, misuse, or damage.
+
+To initiate a refund or exchange, please email us at support@hathibrand.in with your order number and reason for return. We will provide you with a return shipping label and instructions on how to return the item.
+
+Once we receive the returned item, we will inspect it and process the refund or exchange within 5 business days. Refunds will be issued to the original payment method.
+
+If you received a damaged or defective item, please contact us within 7 days of delivery to initiate a return or exchange. We will cover the cost of return shipping in these cases.
+
+Please note that shipping charges are non-refundable, and customers are responsible for the cost of return shipping unless the item is damaged or defective.
+
+If you have any questions or concerns about our refund policy, please contact us at support@hathibrand.in.
+    </div>
+  )
+}
+
+export default RefundPolicy
---
+++ src/Pages/Termsofserice.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Termsofserice() {
+  return (
+    <div>
+        Delivery and Pickup Policies:
+All orders must be placed and paid for in advance. Once an order is received and processed, the customer will be contacted to schedule a delivery or pickup date and time.
+
+For delivery orders, the customer is responsible for providing accurate and complete delivery information, including the delivery address and any special instructions. If the customer is not available to receive the delivery at the scheduled time, they must inform us at least 24 hours in advance to reschedule the delivery. Failure to do so may result in additional delivery charges.
+
+For pickup orders, the customer is responsible for providing a suitable vehicle for transport of the product and securing it properly for safe transportation. The customer is also responsible for adhering to the pickup schedule agreed upon with the factory.
+
+If a delivery or pickup needs to be rescheduled due to unforeseen circumstances, such as weather or mechanical issues, the factory will contact the customer to arrange a new date and time.
+
+Once a delivery or pickup is completed, the customer is responsible for inspecting the product for any damages or defects. Any issues must be reported to the factory within 24 hours of receipt of the product.
+
+The factory is not responsible for any damages or issues that occur during transport or after delivery or pickup is completed. Any damages or issues that occur during transport should be reported to the carrier or transportation company.
+
+By placing an order with the factory, the customer agrees to these delivery and pickup policies.
+    </div>
+  )
+}
+
+export default Termsofserice

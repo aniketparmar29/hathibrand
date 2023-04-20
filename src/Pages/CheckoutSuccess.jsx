43924c89beab0bd5ef30509dc3e6dadc173314e5
aniketparmar29 <aniketparmar068@gmail.com>
2023-04-20 19:10:19 +0530

    AD

--- src/Pages/CheckoutSuccess.jsx
+++ src/Pages/CheckoutSuccess.jsx
@@ -1,40 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
-import { Link } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import { Link, useParams } from 'react-router-dom'; // Importing useParams from react-router-dom
+import Spinner from '../Components/Spinner';
 
 const CheckoutSuccess = () => {
-  const { txn_id } = useParams();
+  const urlSearchParams = new URLSearchParams(window.location.search);
+const client_txn_id = urlSearchParams.get('client_txn_id');
   const [paymentStatus, setPaymentStatus] = useState(null);
+  const [loading, setLoading] = useState(false);
+
   const today = new Date();
-      const day = String(today.getDate()).padStart(2, '0');
-      const month = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
-      const year = today.getFullYear();
-      const formattedDate = `${day}/${month}/${year}`;
-  const bodyop=
-  {
-      "key": "eec16523-acc0-45f8-9b07-f2ac9b34fbd1",
-      "client_txn_id": txn_id,
-      "txn_date": formattedDate
+  const day = String(today.getDate()).padStart(2, '0');
+  const month = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
+  const year = today.getFullYear();
+  const formattedDate = `${day}-${month}-${year}`;
+  const bodyop = {
+    "client_txn_id": client_txn_id,
+    "txn_date": formattedDate
   }
+
   useEffect(() => {
     const checkPaymentStatus = async () => {
+      setLoading(true);
       try {
-        const response = await axios.post(
-          `https://merchant.upigateway.com/api/check_order_status`,
-          bodyop
-        );        
-        const { status } = response.data.data;
+        const response = await fetch(`https://real-cyan-swallow-boot.cyclic.app/check_order`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(bodyop)
+        });
+        const data = await response.json();
+        const { status } = data.data;
         setPaymentStatus(status);
+        setLoading(false);
       } catch (error) {
-        console.log(error);
-        setPaymentStatus('fail');
+        console.log(error.message);
+        setLoading(false);
       }
     };
     checkPaymentStatus();
-  }, [txn_id]);
+  }, [client_txn_id]);
+  
+
 
   return (
     <>
@@ -50,6 +59,11 @@ const CheckoutSuccess = () => {
           Check Orders
         </Link>
       </div>
+      {loading && (
+        <div className="fixed z-50 inset-0 bg-gray-500 opacity-75 flex items-center justify-center">
+          <Spinner />
+        </div>
+      )}
       <Footer />
     </>
   );

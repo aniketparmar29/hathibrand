69c8b33c45b2ca81c480378e4c549ffdb2526ed5
aniketparmar29 <aniketparmar068@gmail.com>
2023-04-24 22:20:03 +0530

    ASD

--- server/server.js
+++ server/server.js
@@ -7,14 +7,15 @@ const cors = require('cors');
 const dotenv = require("dotenv");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const request = require('request');
 
 dotenv.config({path:"./config.env"})
 
 // app.use(cors({
-//   origin: ['http://localhost:3000',''],
-//   credentials: true,
-// }));
-
+  //   origin: ['http://localhost:3000',''],
+  //   credentials: true,
+  // }));
+  
 
 app.use(cors())
 
@@ -57,6 +58,153 @@ pool.getConnection(function(err, connection) {
     });
   });
 
+
+
+  app.post('/create_order', (req, res) => {
+    const data = {
+      key: process.env.API_KEY,
+      client_txn_id: req.body.client_txn_id,
+      amount: req.body.amount,
+      p_info: "products",
+      redirect_url: "https://hathibrand.in/checkoutsuccess",
+      customer_name: req.body.customer_name,
+      customer_email: req.body.customer_email,
+      customer_mobile: req.body.customer_mobile,
+      udf1: "user defined field 1 (max 25 char)",
+      udf2: "user defined field 2 (max 25 char)",
+      udf3: "user defined field 3 (max 25 char)"
+    };
+  
+    request.post({
+      url: "https://merchant.upigateway.com/api/create_order",
+      json: data
+    }, (error, response, body) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).json({
+          status: false,
+          msg: "Internal Server Error"
+        });
+      }
+  
+      if (!body.status) {
+        console.error(body.msg);
+        return res.status(400).json({
+          status: false,
+          msg: body.msg
+        });
+      }
+  
+      const orderId = body.data.order_id;
+      const paymentUrl = body.data.payment_url;
+      let addressop = req.body.addressop;
+      let amount = req.body.amount;
+      let product = req.body.products;
+      let user_id = req.body.user_id;
+      let trx_id = req.body.client_txn_id;
+      const today = new Date();
+      const day = String(today.getDate()).padStart(2, '0');
+      const month = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
+      const year = today.getFullYear();
+      const formattedDate = `${day}/${month}/${year}`;
+      let trx_date = formattedDate;
+      let tracking_id = 0;
+      let payment = false;
+      let status = "pending";
+  
+      pool.query(
+        'INSERT INTO `orders` (`addressop`, `amount`, `product`, `user_id`, `tracking_id`,`trx_id`,`trx_date`,`payment`,`status`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+        [addressop, amount, product, user_id, tracking_id, trx_id, trx_date, payment, status], (err, result) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({
+            status: false,
+            msg: "Internal Server Error"
+          });
+        }
+  
+        console.log("Order details saved to MySQL database");
+  
+        res.json({
+          status: true,
+          msg: "Payment Order Created",
+          data: {
+            order_id: orderId,
+            payment_url: paymentUrl
+          }
+        });
+      });
+    });
+  });
+  
+
+  
+  app.post('/check_order', (req, res) => {
+    const data = {
+      key: process.env.API_KEY,
+      client_txn_id: req.body.client_txn_id,
+      txn_date: req.body.txn_date
+    };
+    console.log(data);
+    request.post({
+      url: "https://merchant.upigateway.com/api/check_order_status",
+      json: data
+    }, (error, response, body) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).json({
+          status: false,
+          msg: "Internal Server Error"
+        });
+      }
+  
+      if (!body.status) {
+        console.error(body.msg);
+        return res.status(400).json({
+          status: false,
+          msg: body.msg
+        });
+      }
+  
+      console.log(body);
+      // Success response
+      return res.status(200).json(body);
+    });
+  });
+  
+
+  app.put('/orders/payment/:trx_id', (req, res) => {
+    const trx_id = req.params.trx_id;
+    const payment = req.body.payment;
+  
+    // execute the SQL query to update the payment status of the order
+    pool.query('UPDATE `orders` SET `payment` = ? WHERE `trx_id` = ?', [payment, trx_id], (error, results, fields) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send('Error updating payment status');
+      } else {
+        res.status(200).send('Payment status updated successfully');
+      }
+    });
+  });
+
+
+  app.get('/orders/:id', function(req, res) {
+    var user_id = req.params.id;
+    pool.query('SELECT * FROM orders WHERE user_id = ?', [user_id], function(error, results, fields) {
+      if (error) throw error;
+      res.send(results);
+    });
+  });
+  
+  app.get('/orders', function(req, res) {
+    pool.query('SELECT * FROM orders', function(error, results, fields) {
+      if (error) throw error;
+      res.send(results);
+    });
+  });
+  
+
   //create product
   app.post('/products/new', (req, res) => {
   
@@ -96,7 +244,7 @@ app.get('/products/:id', (req, res) => {
       res.sendStatus(404); // return a 404 status code if the product is not found
       return;
     }
-    res.json(results[0]); // return the first product in the array
+    res.json(results[0]); 
   });
 });
 
@@ -105,14 +253,9 @@ app.get('/products/:id', (req, res) => {
   ///update product
   app.put('/products/:id', (req, res) => {
     const id = req.params.id;
-    console.log(req.params.id)
-    const { name, price, stock, image, weight, category,reviews } = req.body;
-    
-    if (!name || !price || !stock || !image || !weight || !category || !reviews) { // check if any required field is missing
-      res.status(400).send('Missing required fields');
-      return;
-    }
-  
+    console.log(req.body)
+
+    const { name, price, stock,weight } = req.body;
     let updateFields = '';
     const updateParams = [];
     
@@ -124,14 +267,7 @@ app.get('/products/:id', (req, res) => {
       updateFields += 'price = ?, ';
       updateParams.push(price);
     }
-    if (image) {
-      updateFields += 'image = ?, ';
-      updateParams.push(image);
-    }
-    if (date) {
-      updateFields += 'date = ?, ';
-      updateParams.push(date);
-    }
+
     if (stock) {
       updateFields += 'stock = ?, ';
       updateParams.push(stock);
@@ -139,14 +275,6 @@ app.get('/products/:id', (req, res) => {
     if (weight) {
       updateFields += 'weight = ?, ';
       updateParams.push(weight);
-    }
-    if (category) {
-      updateFields += 'category = ?, ';
-      updateParams.push(category);
-    }
-    if (reviews) {
-      updateFields += 'reviews = ?, ';
-      updateParams.push(reviews);
     }
   
     // remove trailing comma
@@ -259,7 +387,6 @@ app.post('/register', (req, res) => {
       }
 
       const role = 'user';
-
       // Insert new user into MySQL database
       const insertUserSql = 'INSERT INTO users (email, password, name, role) VALUES (?, ?, ?, ?)';
       const insertUserValues = [email, hash, name, role];
@@ -342,7 +469,7 @@ app.post('/login', (req, res) => {
   });
 });
 
-//delete user
+//delete user 
 app.delete('/user/:id', (req, res) => {
   const userid = req.params.id;
 
@@ -373,6 +500,7 @@ app.get('/user/:id', (req, res) => {
   });
 });
 
+
 //get all users
 
 app.get('/users', (req, res) => {
@@ -395,10 +523,12 @@ app.get('/users', (req, res) => {
   });
 });
 
+
+
 //cart 
 app.post('/add-to-cart', (req, res) => {
   console.log(req.body)
-  const { pr_name, pr_price, pr_que, pr_id, pr_img,pr_weight, user_id } = req.body;
+  const { pr_name, pr_price, pr_que, pr_id, pr_img,pr_category,pr_weight, user_id } = req.body;
 
   // check if a row already exists for the given pr_id and user_id combination
   pool.query(
@@ -425,8 +555,8 @@ app.post('/add-to-cart', (req, res) => {
       } else {
         // insert a new row for the product in the cart table
         pool.query(
-          'INSERT INTO cart (pr_name, pr_price, pr_que, pr_id, pr_img,pr_weight, user_id) VALUES (?, ?, ?, ?, ?, ?, ?)',
-          [pr_name, pr_price, pr_que, pr_id, pr_img,pr_weight, user_id],
+          'INSERT INTO cart (pr_name, pr_price, pr_que, pr_id, pr_img,pr_category,pr_weight, user_id) VALUES (?, ?, ?, ?, ?, ?, ?,?)',
+          [pr_name, pr_price, pr_que, pr_id, pr_img,pr_category,pr_weight, user_id],
           (error, results, fields) => {
             if (error) {
               console.error(error);
@@ -481,6 +611,23 @@ app.delete('/cart/:userId/:prId', (req, res) => {
   });
 });
 
+
+app.delete('/cart/:userId', (req, res) => {
+  const userId = req.params.userId;
+
+  pool.query('DELETE FROM cart WHERE user_id = ?', [userId], (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Error deleting items from cart');
+      return;
+    }
+
+    res.send(`Cart cleared for user with user_id ${userId}`);
+  });
+});
+
+
+
 //update_cart
 
 app.put('/cart/:userId/:prId', (req, res) => {
@@ -508,12 +655,12 @@ app.put('/cart/:userId/:prId', (req, res) => {
 
 
 app.post('/reviews/new', (req, res) => {
-  const { user_name, rating, review, pr_id, user_id } = req.body;
+  const { user_name, rating, review, pr_id, pr_name, user_id } = req.body;
 
   // insert the item into the cart table
   pool.query(
-    'INSERT INTO reviews (user_name, rating, review, pr_id, user_id) VALUES (?, ?, ?, ?, ?)',
-    [user_name,rating,review, pr_id, user_id],
+    'INSERT INTO reviews (user_name, rating, review, pr_id, pr_name, user_id) VALUES (?, ?, ?, ?, ?, ?)',
+    [user_name,rating,review, pr_id, pr_name, user_id],
     (error, results, fields) => {
       if (error) {
         console.error(error);
@@ -525,7 +672,24 @@ app.post('/reviews/new', (req, res) => {
   );
 });
 
+// get all reviews
+app.get('/reviews', (req, res) => {
+
+
+  pool.query(`SELECT * FROM reviews `, (err, results) => {
+    if (err) {
+      console.error('Error retrieving reviews:', err);
+      res.sendStatus(500);
+      return;
+    }
+    res.json(results);
+  });
+});
+
+
+
 // get reviews
+
 app.get('/reviews/:pr_id', (req, res) => {
   const pr_id = req.params.pr_id;
 
@@ -647,6 +811,49 @@ app.delete('/coupons/:id', (req, res) => {
     res.send('Coupon deleted successfully.');
   });
 });
+
+//get slider
+app.get('/slider', (req, res) => {
+
+  // Query to get slider data from database 
+  let sql = `SELECT * FROM slider ORDER BY id DESC`;
+
+  pool.query(sql, (err, results) => {
+
+      if(err) throw err;
+
+      res.json(results);
+
+  }); 
+}); 
+
+ // Create new slider data  
+ app.post('/slider', (req, res) => {
+
+  let sqlQuery = `INSERT INTO slider SET ?`;
+
+  let data = {url: req.body.url};
+
+  pool.query(sqlQuery, data, (err, result) => {
+
+      if(err) throw err;
+
+      res.send({message: 'Slider created successfully!'});
+
+  });  
+});
+
+// Delete existing slider data  
+app.delete('/slider/:id', (req, res) => {
+
+  let sqlQuery = `DELETE FROM slider WHERE id=${req.params.id}`;
+
+  pool.query(sqlQuery, (err, result) => {
+
+      if(err) throw err;
+
+      res.send({message: 'Slider deleted successfully!'});    												                                                                                                                            });  
+}); 
 
 
 

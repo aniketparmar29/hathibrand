420291f0041b87d67a7d6a225349c0142ba933a7
aniketparmar29 <aniketparmar068@gmail.com>
2023-04-29 17:44:19 +0530

    ASD

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -32,19 +32,6 @@ function ProductList({ setshowalert, showalert }) {
   
   const handleWeightChange = (event) => {
     const value = event.target.value;
-    if (value === 'ag') {
-      // sort products from high to low
-      setFilterProducts([...product].filter((a, b) => b.weight - a.weight));
-    } else if (value === 'cm') {
-      // sort products from low to high
-      setFilterProducts([...product].sort((a, b) => a.weight - b.weight));
-    } else {
-      // default case, no sorting
-      setFilterProducts([...product]);
-    }
-  };
-  const handleCategoryChange = (event) => {
-    const value = event.target.value;
     if (value === 'htl') {
       // sort products from high to low
       setFilterProducts([...product].sort((a, b) => b.weight - a.weight));
@@ -55,6 +42,16 @@ function ProductList({ setshowalert, showalert }) {
       // default case, no sorting
       setFilterProducts([...product]);
     }
+  };
+  const handleCategoryChange = (event) => {
+    const value = event.target.value;
+    console.log(value)
+    setFilterProducts(product.filter((pro) => {
+      if (value === '') {
+        return true;
+      }
+      return pro.Category === value;
+    }))
   };
   const handlePriceChange = (event) => {
     const value = event.target.value;
@@ -74,47 +71,48 @@ function ProductList({ setshowalert, showalert }) {
 React.useEffect(() => {
   dispatch(getProducts());
 }, [dispatch]);
+console.log(filterProducts)
   return (
     <>
     <div>
-      <div class="lg:flex md:flex  justify-between items-center p-5">
-  <div class="relative">
+      <div className="lg:flex md:flex  justify-between items-center p-5">
+  <div className="relative">
     <select 
       onChange={handleWeightChange} 
-      class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+      className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
     >
       <option value="">Sort by weight</option>
       <option value="htl">High to low</option>
       <option value="lth">Low to high</option>
     </select>
-    <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-      <svg class="fill-current h-4 w-4" viewBox="0 0 20 20"><path d="M10 12l-5-5 1.41-1.41L10 9.17l3.59-3.58L15 7l-5 5z"/></svg>
+    <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+      <svg className="fill-current h-4 w-4" viewBox="0 0 20 20"><path d="M10 12l-5-5 1.41-1.41L10 9.17l3.59-3.58L15 7l-5 5z"/></svg>
     </div>
   </div>
-  <div class="relative">
+  <div className="relative">
     <select 
       onChange={handleCategoryChange} 
-      class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+      className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
     >
       <option value="">Filter by Category</option>
-      <option value="ag">Aggarbatti</option>
-      <option value="cm">Cosmetic</option>
+      <option value="Agarbatti">Agarbatti</option>
+      <option value="Cosmetic">Cosmetic</option>
     </select>
-    <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-      <svg class="fill-current h-4 w-4" viewBox="0 0 20 20"><path d="M10 12l-5-5 1.41-1.41L10 9.17l3.59-3.58L15 7l-5 5z"/></svg>
+    <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+      <svg className="fill-current h-4 w-4" viewBox="0 0 20 20"><path d="M10 12l-5-5 1.41-1.41L10 9.17l3.59-3.58L15 7l-5 5z"/></svg>
     </div>
   </div>
-  <div class="relative">
+  <div className="relative">
     <select 
       onChange={handlePriceChange} 
-      class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+      className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
     >
       <option value="">Sort by price</option>
       <option value="htl">High to low</option>
       <option value="lth">Low to high</option>
     </select>
-    <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-      <svg class="fill-current h-4 w-4" viewBox="0 0 20 20"><path d="M10 12l-5-5 1.41-1.41L10 9.17l3.59-3.58L15 7l-5 5z"/></svg>
+    <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+      <svg className="fill-current h-4 w-4" viewBox="0 0 20 20"><path d="M10 12l-5-5 1.41-1.41L10 9.17l3.59-3.58L15 7l-5 5z"/></svg>
     </div>
   </div>
 </div>
